61a811aecbc877acae3e8691b5beed48a3034ef8
impr3ssi0n <30906761+impr3ssi0n@users.noreply.github.com>
2025-07-06 18:15:54 +0300

    Update lme_imp.js

--- lme_imp.js
+++ lme_imp.js
@@ -1,25 +1,4 @@
 (function () {
     'use strict';
-
-    // var manifest = {
-    //   type: "other",
-    //   version: "0.0.5",
-    //   author: '@lme_chat',
-    //   name: "Lampa Movie Enhancer",
-    //   description: "Some tweaks for movie content",
-    //   component: "lme"
-    // };
-
-    function startPlugin() {
-        window.own_rating = true;
-        Lampa.Listener.follow('full', function (e) {
-            if (e.type == 'complite') {
-                var render = e.object.activity.render();
-                console.log(render);
-            };
-        });
-    };
-
-    if (!window.own_rating)
-        startPlugin();
+    Lampa.Utils.putScriptAsync(['https://b557-23-157-88-114.ngrok-free.app/ownrating.js'], function () { });
 })();
