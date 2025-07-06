ad58181b3ca6854611e839747cae8e28f362941a
impr3ssi0n <30906761+impr3ssi0n@users.noreply.github.com>
2025-07-06 18:25:37 +0300

    Update lme_imp.js

--- lme_imp.js
+++ lme_imp.js
@@ -1,4 +1,4 @@
 (function () {
     'use strict';
-    Lampa.Utils.putScriptAsync(['https://b557-23-157-88-114.ngrok-free.app/ownrating.js'], function () { });
+    Lampa.Utils.putScriptAsync(['https:///ownrating.js'], function () { });
 })();
